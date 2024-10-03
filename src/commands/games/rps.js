b52b30069976884ad76c31c43b256e76b8c8ceb3
XeonLatte <austin960924@gmail.com>
2024-10-03 20:22:17 +0800

    add a command, improve a command
    rps embed message
    add avatar.js

--- src/commands/games/rps.js
+++ src/commands/games/rps.js
@@ -1,4 +1,4 @@
-const { ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, SlashCommandBuilder, ComponentType } = require('discord.js');
+const { ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, SlashCommandBuilder, ComponentType, EmbedBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -8,7 +8,7 @@ module.exports = {
 	async execute(interaction) {
 		const selections = new StringSelectMenuBuilder()
 			.setCustomId('rps')
-			.setPlaceholder('Make a selection!')
+			.setPlaceholder('Rock, Paper or Scissor')
 			.addOptions(
 				new StringSelectMenuOptionBuilder()
 					.setLabel('Rock')
@@ -24,14 +24,23 @@ module.exports = {
 		const row = new ActionRowBuilder()
 			.addComponents(selections);
 
+        const embed1 = new EmbedBuilder()
+            .setTitle(':video_game: | Rock Paper Scissor')
+            .setDescription('Choose an option below.')
+            .setColor('#00b0f4')
+            .setFooter({
+                text: 'Amethyst • a functional discord bot',
+                iconURL: 'https://i.imgur.com/7H8rGZs.png',
+        });
+
 		const response = await interaction.reply({
-			content: 'Rock, Paper, Scissor!',
+            embeds: [embed1],
 			components: [row],
 		});
 
         const collector = response.createMessageComponentCollector({ componentType: ComponentType.StringSelect, time: 3_600_000 });
         const options = ['rock', 'paper', 'scissor'];
-        const botChoice = options[Math.round(Math.random() * 3)];
+        const botChoice = options[Math.round(Math.random() * 2)];
 
         collector.on('collect', async input => {
             const value = input.values[0];
@@ -50,9 +59,18 @@ module.exports = {
                 result = 'You Win!'
             }
 
+            const embed2 = new EmbedBuilder()
+                .setTitle(':video_game: | Rock Paper Scissor')
+                .setDescription(`Your choice: ${value}\nBot choice: ${botChoice}\n\n**${result}**`)
+                .setColor('#00b0f4')
+                .setFooter({
+                    text: 'Amethyst • a functional discord bot',
+                    iconURL: 'https://i.imgur.com/7H8rGZs.png',
+            });
+
             selections.setDisabled(true);
 
-            await input.reply({ content: `Your choice: ${value}\nBot choice: ${botChoice}\n${result}`});
+            await input.reply({ embeds: [embed2] });
             await interaction.editReply({ components: [row] });
         });
 	},
---
+++ src/commands/misc/avatar.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('avatar')
+		.setDescription('Provides user avatar.')
+        .addUserOption( option =>
+            option.setName('user')
+                .setDescription('The user you want to display.')
+                .setRequired(true)),  
+    
+	async execute(interaction) {
+		const target = interaction.options.getUser('user');
+
+        const embed = new EmbedBuilder()
+            .setTitle(`${target.tag}'s avatar`)
+            .setImage(target.displayAvatarURL({ size: 2048 }))
+            .setColor('#00b0f4')
+            .setFooter({
+                text: 'Amethyst • a functional discord bot',
+                iconURL: 'https://i.imgur.com/7H8rGZs.png',
+        });
+      
+      await interaction.reply({ embeds: [embed] });
+
+	},
+};
